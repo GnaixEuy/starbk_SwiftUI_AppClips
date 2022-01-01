89304fed985d174372044a71e0e8265d9cf61e9f
GnaixEuy <sgghcj@live.com>
2022-01-01 21:24:36 +0800

    Initial Commit

---
+++ Shared/starbuckApp.swift
@@ -0,0 +1,17 @@
+//
+//  starbuckApp.swift
+//  Shared
+//
+//  Created by 苏粤翔 on 2022/1/1.
+//
+
+import SwiftUI
+
+@main
+struct starbuckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
