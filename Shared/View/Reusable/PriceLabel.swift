da92411b9ad8289bf5119f2e8ceee4f9dfce79dc
GnaixEuy <sgghcj@live.com>
2022-01-01 21:36:14 +0800

    init project

---
+++ Shared/View/Reusable/PriceLabel.swift
@@ -0,0 +1,20 @@
+//
+//  PriceLabel.swift
+//  starbuck (iOS)
+//
+//  Created by 苏粤翔 on 2022/1/1.
+//
+
+import SwiftUI
+
+struct PriceLabel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PriceLabel_Previews: PreviewProvider {
+    static var previews: some View {
+        PriceLabel()
+    }
+}
