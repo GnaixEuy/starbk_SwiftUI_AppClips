3e197d7f337c2935436c668ae8c9143fe4b6d6d4
GnaixEuy <sgghcj@live.com>
2022-01-03 19:12:58 +0800

    :beers: 添加了商品图片View和网络请求地址接口

---
+++ Shared/Data/Live/Network.swift
@@ -0,0 +1,19 @@
+//
+//  NetWork.swift
+//  starbuck (iOS)
+//
+//  Created by 苏粤翔 on 2022/1/3.
+//
+
+import Foundation
+
+let baseUrl = "http://localhost:1337"
+
+struct Network {
+    static let findDrinks  = baseUrl     + "/drinks"
+    static let createOrder = baseUrl     + "/orders"
+    static let findOrders  = createOrder
+    static let deleteOrder = createOrder + "/"
+    static let updateOrder = createOrder + "/"
+}
+    
---
+++ Shared/View/Home/MenuCoverView.swift
@@ -0,0 +1,46 @@
+//
+//  MenuCoverView.swift
+//  starbuck (iOS)
+//
+//  Created by 苏粤翔 on 2022/1/3.
+//
+
+import SwiftUI
+import SDWebImageSwiftUI
+
+struct MenuCoverView: View {
+    let imgUrl : String
+    
+    
+    var body: some View {
+        ZStack{
+            if Bool.random(){
+                Image("coffeeMask")
+                    .renderingMode(.template)
+                    .foregroundColor(Color("coverMask"))
+            }else{
+                RoundedRectangle(cornerRadius: 16)
+                    .frame(width: 270, height: 340)
+                    .foregroundColor(Color("coverMask"))
+            }
+            
+            WebImage(url: URL(string: baseUrl + imgUrl))
+                .resizable()
+                .scaledToFit()
+                .frame(width: 230)
+        }
+    }
+}
+
+struct MenuCoverView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            MenuCoverView(imgUrl: drinksData[0].menus[0].image[0].url)
+                .preferredColorScheme(.dark)
+                .previewLayout(.sizeThatFits)
+            MenuCoverView(imgUrl: drinksData[0].menus[2].image[0].url)
+                .preferredColorScheme(.dark)
+                .previewLayout(.sizeThatFits)
+        }
+    }
+}
